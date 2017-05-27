a087ebe4caf512f15dba6e3b634ae4ce1a2c6b90
Konstantin Krupovich <kkrupovich@gmail.com>
2017-05-27 16:41:07 +0300

    - [+] итоговый вариант прототипа;

---
+++ EmerShool/PupilTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PupilTableViewController.h
+//  EmerShool
+//
+//  Created by Konstantin Krupovich on 27/05/2017.
+//  Copyright © 2017 EmerShool. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface PupilTableViewController : BaseTableViewController
+
+@end
